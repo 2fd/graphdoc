0591e8c074e9ffeae495c06f1411f321ac9afebd
Fede Ramirez <i@2fd.me>
2016-08-17 01:02:57 -0300

    add scalar test example

--- test/schema.ts
+++ test/schema.ts
@@ -1,4 +1,12 @@
-import {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString} from 'graphql';
+import {GraphQLScalarType, GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString} from 'graphql';
+
+let Timestamp = new GraphQLScalarType({
+    name: 'Timestamp',
+    serialize: (value) => value;
+    parseValue: (value) => value;
+    parseLiteral: (ast) => ast.value;
+});
+
 
 export const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -7,7 +15,7 @@ export const schema = new GraphQLSchema({
         fields: {
             timestamp: {
                 description: 'Unix timestamp',
-                type: GraphQLInt,
+                type: Timestamp,
                 resolve: () => Date.now()
             },
             datetime: {
