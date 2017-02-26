b5ef758cfa20a816e985bea992b4e2e8acc0378a
Fede Ramirez <i@2fd.me>
2017-02-25 22:33:07 -0300

    add mobile support

--- lib/utility/template.ts
+++ lib/utility/template.ts
@@ -29,7 +29,7 @@ export function createData(
             return {
                 title,
                 description,
-                headers,
+                headers: headers.join(''),
                 navigations,
                 documents,
                 projectPackage,
