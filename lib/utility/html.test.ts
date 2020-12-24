ab6650ed37a43699473e423d0f1a4c836ab97ad4
Fede <i@2fd.me>
2020-12-24 19:21:02 -0300

    test: add tests

--- lib/utility/html.test.ts
+++ lib/utility/html.test.ts
@@ -1,4 +1,4 @@
-import { split } from './html'
+import { split, HTML } from './html'
 
 test('utility/html.split', () => {
   const LOREM_IPSU = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
@@ -29,4 +29,44 @@ test('utility/html.split', () => {
     "standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
     "it to make a type specimen book.",
   ])
+})
+
+
+describe('lib/utility/html#HTML', () => {
+  test('.code', () => {
+    const html = new HTML
+    expect(html.code('CODE')).toBe('<code class="highlight"><table class="code"><tbody>CODE</tbody></table></code>')
+  })
+
+  test('.highlight', () => {
+    const html = new HTML
+    expect(html.highlight('CODE')).toBe('<strong>CODE</strong>')
+  })
+
+  test('.sup', () => {
+    const html = new HTML
+    expect(html.sup('CODE')).toBe(' <sup>CODE</sup>')
+  })
+
+  test('.line', () => {
+    const html = new HTML
+    expect(html.index).toBe(1)
+    expect(html.line('CODE')).toBe('<tr class="row"><td id="L1" class="td-index">1</td><td id="LC1" class="td-code">CODE</td></tr>')
+    expect(html.index).toBe(2)
+  })
+
+  test('.tab', () => {
+    const html = new HTML
+    expect(html.tab('CODE')).toBe('<span class="tab">CODE</span>')
+  })
+
+  test('.keyword', () => {
+    const html = new HTML
+    expect(html.keyword('CODE')).toBe('<span class="keyword operator ts">CODE</span>')
+  })
+
+  test('.comment', () => {
+    const html = new HTML
+    expect(html.comment('CODE')).toBe('<span class="comment line"># CODE</span>')
+  })
 })
