e5e325f4818a51434e9a70f4365513ac64106bdb
Fede <i@2fd.me>
2020-12-26 16:40:48 -0300

    test: add test

--- lib/utility/html.test.ts
+++ lib/utility/html.test.ts
@@ -1,4 +1,6 @@
+import { Description, Field, InputValue, SchemaType } from '../interface'
 import { split, HTML } from './html'
+import { data } from '../../test/empty.schema.json'
 
 test('utility/html.split', () => {
   const LOREM_IPSU = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
@@ -68,5 +70,44 @@ describe('lib/utility/html#HTML', () => {
   test('.comment', () => {
     const html = new HTML
     expect(html.comment('CODE')).toBe('<span class="comment line"># CODE</span>')
+  })
+
+  test('.identifier', () => {
+    const html = new HTML
+    const type = data.__schema.types.find(t => t.name === 'Query')
+    expect(html.identifier(type as Description)).toBe('<span class="identifier">Query</span>')
+  })
+
+  test('.parameter', () => {
+    const html = new HTML
+    const input: InputValue = data.__schema.types.find(t => t.name === 'AddCommentInput') as any
+    expect(html.parameter(input)).toBe('<span class="variable parameter">AddCommentInput</span>')
+  })
+
+  test('.property', () => {
+    const html = new HTML
+    expect(html.property('PROPERTY')).toBe('<span class="meta">PROPERTY</span>')
+  })
+
+  test('.useIdentifier', () => {
+    const html = new HTML
+    const schema: SchemaType = data.__schema.types.find(t => t.name === '__Schema') as any
+    const field: Field = (schema.fields || []).find(f => f.name === 'types') as any
+
+    expect(html.useIdentifier(field.type, 'HREF')).toBe('[<a class="support type" href="HREF">__Type</a>!]!')
+  })
+
+  test('.useIdentifierLength', () => {
+    const html = new HTML
+    const schema: SchemaType = data.__schema.types.find(t => t.name === '__Schema') as any
+    const field: Field = (schema.fields || []).find(f => f.name === 'types') as any
+
+    expect(html.useIdentifierLength(field.type)).toBe(10)
+  })
+
+  test('.value', () => {
+    const html = new HTML
+    expect(html.value('"STRING"')).toBe('<span class="string">"STRING"</span>')
+    expect(html.value('NUMBER')).toBe('<span class="constant numeric">NUMBER</span>')
   })
 })
